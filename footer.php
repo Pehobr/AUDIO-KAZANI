5e85a48bb2c9f5ebd49a900c30d3d84739c691bd
Pehobr <pehobr@gmail.com>
2025-08-25 08:31:37 +0200

    LADĚNÍ

--- footer.php
+++ footer.php
@@ -45,7 +45,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.246 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
             </svg>
         </a>
-        <a href="/informace/" class="nav-item">
+        <a href="/info/" class="nav-item">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
@@ -70,6 +70,8 @@
     align-items: center;
     box-shadow: 0 -2px 10px rgba(0,0,0,0.2);
     z-index: 1000;
+    /* VRÁCENÝ STYL: Horní ohraničení */
+    border-top: 1px solid rgba(255, 255, 255, 0.2);
 }
 
 .mobile-bottom-nav .nav-item {
@@ -84,10 +86,10 @@
     transition: background-color 0.2s ease;
 }
 
-/* NOVÝ STYL: Sjednocení velikosti ikon */
+/* Sjednocení velikosti ikon */
 .mobile-bottom-nav .nav-item svg {
-    width: 30px;
-    height: 30px;
+    width: 24px;
+    height: 24px;
 }
 
 .mobile-bottom-nav .nav-item:hover {
